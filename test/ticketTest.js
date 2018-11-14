59daa06907103ccba9e4df85066b85a6120d1805
daler96 <daler0696@gmail.com>
2018-11-14 18:23:25 +0300

    initial tests with one ticket based sample

--- test/ticketTest.js
+++ test/ticketTest.js
@@ -10,12 +10,16 @@ const truffleAssert = require("truffle-assertions");
 
 const dEventTicket = artifacts.require("dEventTicket");
 
-contract("Lender Registry", accounts => {
-    const colendiGlobe = accounts[0];
-    const userIstan = accounts[1];
-    const userAnka = accounts[2];
-    const userIzm = accounts[3];
-    const userAda = accounts[4];
+contract("Ticket Contract", accounts => {
+
+    const ticketCreator = accounts[1];
+    const Alice = accounts[2];
+    const Bob = accounts[3];
+    const ticketAmount = 100;
+    const ticketPrice = 2e17
+    let buyerCount = 0
+
+
 
     before(async () => {
         dEventInstance = await dEventTicket.new();
@@ -23,54 +27,50 @@ contract("Lender Registry", accounts => {
 
 
     it("Ticket created", async () => {
-        const senderBalanceBefore = await web3.eth.getBalance(userIstan)
 
         await dEventInstance.createTicket.sendTransaction(
             "Cinemaximum ticket",
-            2e17,
-            100,
+            ticketPrice,
+            ticketAmount,
             {
-                from: userIstan
+                from: ticketCreator,
+                gasPrice: 0
             });
 
         const sizeOfTickets = await dEventInstance.getTicketsLength.call();
         sizeOfTickets.should.be.bignumber.eq(1)
 
-        //const receiverBalanceBefore = await web3.eth.getBalance(userIstan)
-        //console.log(receiverBalanceBefore)
-
     });
 
     it("Ticket bought", async () => {
-        const userAnkaBalance = await web3.eth.getBalance(userAnka)
-        console.log(userAnkaBalance.toNumber())
+        const AliceBalance = await web3.eth.getBalance(Alice)
+        const ticketInfo = await dEventInstance.getTicket.call(0);
+
         await dEventInstance.payForTicket.sendTransaction(
             0,
             {
-                from: userAnka,
-                value: 2e17
+                from: Alice,
+                value: ticketPrice,
+                gasPrice: 0
             });
 
-        const receiverBalanceBefore = await web3.eth.getBalance(userAnka)
-        console.log(receiverBalanceBefore.toNumber())
-        const tickets = await dEventInstance.getMyTickets.call({ from: userAnka })
-        console.log(tickets)
+        const AliceBalanceAfter = await web3.eth.getBalance(Alice)
+        buyerCount++;
+        AliceBalanceAfter.should.be.bignumber.eq(AliceBalance.sub(ticketPrice));
     });
 
     it("Event creator gets Money Back", async () => {
-        const userIstanBalance = await web3.eth.getBalance(userIstan)
-        console.log(userIstanBalance.toNumber())
+        const ticketCreatorBalance = await web3.eth.getBalance(ticketCreator)
+
         await dEventInstance.getMoneyBack.sendTransaction(
             0,
             {
-                from: userAnka,
-                value: 2e17
+                from: ticketCreator,
+                gasPrice: 0
             });
 
-        const receiverBalanceBefore = await web3.eth.getBalance(userAnka)
-        console.log(receiverBalanceBefore.toNumber())
-        const tickets = await dEventInstance.getMyTickets.call({ from: userAnka })
-        console.log(tickets)
+        const ticketCreatorBalanceAfter = await web3.eth.getBalance(ticketCreator)
+        ticketCreatorBalanceAfter.should.be.bignumber.eq(ticketCreatorBalance.add(ticketPrice))
     });
 
 
